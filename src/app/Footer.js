56ca994d2084ee0bc9b306446eb98cab6509de84
Haseeb Abid <haseebabid72@outlook.com>
2024-05-28 13:40:39 +0500

    Did a few adjustments

---
+++ src/app/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      Hehe
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/app/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      Hye Man
+    </div>
+  )
+}
+
+export default Navbar
